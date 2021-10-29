2583513f9cf6a6ca8a8adc03c82020b6faacf9f0
Nick Allen <rutherford.t.b@gmail.com>
2021-10-29 16:54:34 -0500

    import index reqs from redux, react-redux, redux-logger

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
