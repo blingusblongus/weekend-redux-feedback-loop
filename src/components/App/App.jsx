77e0ca4edf0aed8b103ed3aa3dd1696b880a9ae6
Nick Allen <rutherford.t.b@gmail.com>
2021-10-30 13:55:22 -0500

    next button routing correctly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,23 +4,43 @@ import './App.css';
 import Review from '../Review/Review';
 import Form from '../Form/Form.jsx';
 import Admin from '../Admin/Admin';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-      <main>
-        <Form formSection="feeling" />
-        <Form formSection="understanding" />
-        <Form formSection="support" />
-        <Form formSection="comments" />
-        <Review />
-        <Admin />
-      </main>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <main>
+          <Route path="/" exact>
+            <Form formSection="feeling" />
+          </Route>
+
+          <Route path="/understanding">
+            <Form formSection="understanding" />
+          </Route>
+
+          <Route path="/support">
+            <Form formSection="support" />
+          </Route>
+
+          <Route path="/comments">
+            <Form formSection="comments" />
+          </Route>
+
+          <Route path="/review">
+            <Review />
+          </Route>
+          
+          <Route path="/admin">
+            <Admin />
+          </Route>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,30 +1,36 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
 
 function FormFeeling({formSection}) {
     const formData = useSelector(store => store.feedbackReducer);
     const [input, setInput] = useState(formData[formSection] || '');
     const dispatch = useDispatch();
-    let prompt, inputType;
+    const history = useHistory();
+    let prompt, inputType, back, next;
 
     // Select form content
     switch (formSection) {
         case 'feeling':
             prompt = "How are you feeling today?";
             inputType = 'number';
+            next = '/understanding';
             break;
         case 'understanding':
             prompt = "How well are you understanding the content?";
             inputType = 'number';
+            next = '/support';
             break;
         case 'support':
             prompt = "How well are you being supported?";
             inputType = 'number';
+            next = '/comments';
             break;
         case 'comments':
             prompt = "Any comments you want to leave?";
             inputType = 'text';
+            next = '/review';
             break;
         default:
             break;
@@ -41,7 +47,9 @@ function FormFeeling({formSection}) {
                 formSection: formSection,
                 value: input
             }
-        })
+        });
+
+        history.push(next)
     }
 
     return (
