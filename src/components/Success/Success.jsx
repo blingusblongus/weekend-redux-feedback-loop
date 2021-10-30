a6c29bdc303070a0964e9d3a96a514aadb06b354
Nick Allen <rutherford.t.b@gmail.com>
2021-10-30 15:06:41 -0500

    add back button to Review, clear fields on submit

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router";
+
 function Success(props) {
+    const history = useHistory();
+    
     return (
         <div>
             <h2>Thanks for your feedback!</h2>
             <p>Your responses have been recorded</p>
+            <button onClick={() => history.push('/')}>Leave New Feedback</button>
         </div>
         
     )
