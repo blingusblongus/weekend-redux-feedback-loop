e2530a8f9f8ae41d5216e366622e51af3f607255
Nick Allen <rutherford.t.b@gmail.com>
2021-10-29 21:08:10 -0500

    added admin component

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,20 @@
+function Admin(props) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Feeling</th>
+                    <th>Understanding</th>
+                    <th>Support</th>
+                    <th>Comments</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    )
+}
+
+export default Admin;
