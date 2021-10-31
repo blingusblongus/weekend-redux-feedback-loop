5a639445ee1d29249bec9a6c7e11c4f13486a204
Nick Allen <rutherford.t.b@gmail.com>
2021-10-30 23:59:47 -0500

    non-destructive pruning

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -5,10 +5,9 @@ import AdminItem from "../AdminItem/AdminItem";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { Button, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import { Paper } from "@mui/material";
 
 function Admin(props) {
@@ -28,8 +27,6 @@ function Admin(props) {
                 console.log('Admin GET ERR')
             );
     }
-
-    console.log('feedback', feedback);
 
     return (
         <Container>
